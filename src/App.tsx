45fe7f6bef4bdbcf1de6b230d4d3226f51d55eae
Andrew5-Dev <topchiy.ap@gmail.com>
2024-08-29 11:24:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+    return (
+        <>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <p>Click on the Vite and React logos to learn more</p>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default App
